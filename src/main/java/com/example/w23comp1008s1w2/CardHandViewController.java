9c37775d9cbf261c36908e72fd0f91b86372ff6d
JaretWright <jaret.wright@georgiancollege.ca>
2023-01-26 17:04:59 -0500

    Week 3 - Updated the view and CardApplication.java class to load the improved view

---
+++ src/main/java/com/example/w23comp1008s1w2/CardHandViewController.java
@@ -0,0 +1,30 @@
+package com.example.w23comp1008s1w2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class CardHandViewController {
+
+    @FXML
+    private Label colourLabel;
+
+    @FXML
+    private Label faceNameLabel;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Label suitLabel;
+
+    @FXML
+    private Label valueLabel;
+
+    @FXML
+    void dealNextCard(ActionEvent event) {
+
+    }
+
+}
